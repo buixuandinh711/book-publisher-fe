ef52c2c89563321a4a4d6a459e3350d26d11b8ab
dinhbx <dinhbx@smartosc.com>
2023-04-12 17:50:13 +0700

    init HomeSlider

---
+++ components/HomeSlider/index.tsx
@@ -0,0 +1,139 @@
+export function HomeSlider() {
+  return (
+    <section className="lib-section-1">
+      <section className="section_slider fw">
+        <div
+          className="home-slider owl-carousel not-dqowl owl-loaded owl-drag"
+        >
+          <div className="owl-stage-outer">
+            <div className="owl-stage">
+              <div className="owl-item cloned">
+                <div className="item image-item">
+                  <a
+                    href="https://nxbvanhoc.com.vn/search?query=Tinh+hoa+v%C4%83n+ch%C6%B0%C6%A1ng+vi%E1%BB%87t&amp;type=article%2Cproduct"
+                    title="Tinh hoa văn chương Việt"
+                    className="clearfix"
+                  >
+                    <img
+                      className="img-responsive"
+                      src="//bizweb.dktcdn.net/100/370/339/themes/744741/assets/slider_3_image.jpg?1632297125018"
+                      alt="tinh-hoa-van-chuong-viet"
+                    />
+                  </a>
+                </div>
+              </div>
+              <div className="owl-item cloned">
+                <div className="item image-item">
+                  <a
+                    href="https://nxbvanhoc.com.vn/van-hoc-kinh-dien"
+                    title="Văn học kinh điển"
+                    className="clearfix"
+                  >
+                    <img
+                      className="img-responsive"
+                      src="//bizweb.dktcdn.net/100/370/339/themes/744741/assets/slider_4_image.jpg?1632297125018"
+                      alt="van-hoc-kinh-dien"
+                    />
+                  </a>
+                </div>
+              </div>
+              <div className="owl-item">
+                <div className="item image-item">
+                  <a
+                    href="https://nxbvanhoc.com.vn/truyen-tranh-kinh-dien"
+                    title="Truyện cổ tích kinh điển"
+                    className="clearfix"
+                  >
+                    <img
+                      className="img-responsive"
+                      src="//bizweb.dktcdn.net/100/370/339/themes/744741/assets/slider_1_image.jpg?1632297125018"
+                      alt="truyen-co-tich-kinh-dien"
+                    />
+                  </a>
+                </div>
+              </div>
+              <div className="owl-item">
+                <div className="item image-item">
+                  <a
+                    href="https://nxbvanhoc.com.vn/combo-cung-be-kham-pha-tac-pham-kinh-dien"
+                    className="clearfix"
+                  >
+                    <img
+                      className="img-responsive"
+                      src="//bizweb.dktcdn.net/100/370/339/themes/744741/assets/slider_2_image.jpg?1632297125018"
+                      alt="tu-dai-danh-tac-phien-ban-nhi"
+                    />
+                  </a>
+                </div>
+              </div>
+              <div className="owl-item">
+                <div className="item image-item">
+                  <a
+                    href="https://nxbvanhoc.com.vn/search?query=Tinh+hoa+v%C4%83n+ch%C6%B0%C6%A1ng+vi%E1%BB%87t&amp;type=article%2Cproduct"
+                    title="Tinh hoa văn chương Việt"
+                    className="clearfix"
+                  >
+                    <img
+                      className="img-responsive"
+                      src="//bizweb.dktcdn.net/100/370/339/themes/744741/assets/slider_3_image.jpg?1632297125018"
+                      alt="tinh-hoa-van-chuong-viet"
+                    />
+                  </a>
+                </div>
+              </div>
+              <div className="owl-item active">
+                <div className="item image-item">
+                  <a
+                    href="https://nxbvanhoc.com.vn/van-hoc-kinh-dien"
+                    title="Văn học kinh điển"
+                    className="clearfix"
+                  >
+                    <img
+                      className="img-responsive"
+                      src="//bizweb.dktcdn.net/100/370/339/themes/744741/assets/slider_4_image.jpg?1632297125018"
+                      alt="van-hoc-kinh-dien"
+                    />
+                  </a>
+                </div>
+              </div>
+              <div className="owl-item cloned">
+                <div className="item image-item">
+                  <a
+                    href="https://nxbvanhoc.com.vn/truyen-tranh-kinh-dien"
+                    title="Truyện cổ tích kinh điển"
+                    className="clearfix"
+                  >
+                    <img
+                      className="img-responsive"
+                      src="//bizweb.dktcdn.net/100/370/339/themes/744741/assets/slider_1_image.jpg?1632297125018"
+                      alt="truyen-co-tich-kinh-dien"
+                    />
+                  </a>
+                </div>
+              </div>
+              <div className="owl-item cloned">
+                <div className="item image-item">
+                  <a
+                    href="https://nxbvanhoc.com.vn/combo-cung-be-kham-pha-tac-pham-kinh-dien"
+                    className="clearfix"
+                  >
+                    <img
+                      className="img-responsive"
+                      src="//bizweb.dktcdn.net/100/370/339/themes/744741/assets/slider_2_image.jpg?1632297125018"
+                      alt="tu-dai-danh-tac-phien-ban-nhi"
+                    />
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="owl-nav">
+            <div className="owl-prev">prev</div>
+            <div className="owl-next">next</div>
+          </div>
+          <div className="owl-dots disabled"></div>
+        </div>
+      </section>
+    </section>
+  );
+}
