fc1fd293c0e01935d5abde68ee42ef1eb80d9f74
dinhbx <dinhbx@smartosc.com>
2023-04-24 14:06:15 +0700

    add login page

---
+++ pages/account/login.tsx
@@ -0,0 +1,11 @@
+import { BreadScumb } from "@/components/BreadCrumb";
+import { LoginMain } from "@/components/login/LoginMain";
+
+export default function Catalog() {
+  return (
+    <>
+      <BreadScumb />
+      <LoginMain />
+    </>
+  );
+}
