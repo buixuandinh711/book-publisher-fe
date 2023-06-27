8302924478843a50eb7c8533cccfb153c679d280
buixuandinh711 <buixuandinh711@gmail.com>
2023-06-28 00:46:21 +0700

    add checkout cart info

--- components/checkout/CheckoutOrderInfo/index.tsx
+++ components/checkout/CheckoutOrderInfo/index.tsx
@@ -1,65 +1,42 @@
+import { useCartQuery } from "@/contexts/slices/apiSlice";
 import Image from "next/image";
 
-const listItem = [
-  {
-    name: "Book 1",
-    image: "https://res.cloudinary.com/dsy1fdqx2/image/upload/v1683188454/book-publisher/dong-song-ben-chua.jpg",
-    price: 20000,
-    quantity: 2,
-  },
-  {
-    name: "Book 1",
-    image: "https://res.cloudinary.com/dsy1fdqx2/image/upload/v1683188454/book-publisher/dong-song-ben-chua.jpg",
-    price: 20000,
-    quantity: 2,
-  },
-  {
-    name: "Book 1",
-    image: "https://res.cloudinary.com/dsy1fdqx2/image/upload/v1683188454/book-publisher/dong-song-ben-chua.jpg",
-    price: 20000,
-    quantity: 2,
-  },
-  {
-    name: "Book 1",
-    image: "https://res.cloudinary.com/dsy1fdqx2/image/upload/v1683188454/book-publisher/dong-song-ben-chua.jpg",
-    price: 20000,
-    quantity: 2,
-  },
-];
-
 export function CheckoutOrderInfo() {
+  const cartQuery = useCartQuery({});
+
   return (
     <>
       <div className="flex items-center justify-between pb-4">
         <h2 className="w-full text-xl font-bold">Your cart</h2>
         <div className="flex h-6 w-6 items-center justify-center rounded-full bg-red-700 text-white">
-          <span className="text-center align-middle font-bold">{listItem.length}</span>
+          <span className="text-center align-middle font-bold">{cartQuery.isSuccess ? cartQuery.data.length : 0}</span>
         </div>
       </div>
       <div className="max-h-52 overflow-y-auto rounded-md border border-gray-300 py-2 scrollbar-thin scrollbar-track-gray-100 scrollbar-thumb-gray-300">
-        {listItem.map((item, index) => (
-          <div key={index} className="flex border-b border-gray-300 p-2 first:rounded-t-md last:border-b-0 hover:bg-gray-50">
-            <Image
-              alt={item.name}
-              src={item.image}
-              width="0"
-              height="0"
-              sizes="10vw"
-              className="h-20 w-20 min-w-[80px] object-contain align-middle"
-            />
-            <div className="flex w-full justify-between">
-              <div>
-                <div className="font-semibold">{item.name}</div>
-                <div className="text-xs">
-                  <span>{item.price.toLocaleString()}</span>
-                  {" × "}
-                  <span>{item.quantity}</span>
+        {cartQuery.isSuccess &&
+          cartQuery.data.map((item) => (
+            <div key={item.book.id} className="flex border-b border-gray-300 p-2 first:rounded-t-md last:border-b-0 hover:bg-gray-50">
+              <Image
+                alt={item.book.name}
+                src={item.book.image}
+                width="0"
+                height="0"
+                sizes="10vw"
+                className="h-20 w-20 min-w-[80px] object-contain align-middle"
+              />
+              <div className="flex w-full justify-between">
+                <div>
+                  <div className="line-clamp-3 font-semibold">{item.book.name}</div>
+                  <div className="text-xs">
+                    <span>{item.book.currentPrice.toLocaleString()}</span>
+                    {" × "}
+                    <span>{item.quantity}</span>
+                  </div>
                 </div>
+                <div className="font-medium">{(item.quantity * item.book.currentPrice).toLocaleString()}$</div>
               </div>
-              <div className="font-medium">{(item.quantity * item.price).toLocaleString()}$</div>
             </div>
-          </div>
-        ))}
+          ))}
       </div>
     </>
   );
--- components/checkout/CheckoutTotal/index.tsx
+++ components/checkout/CheckoutTotal/index.tsx
@@ -1,9 +1,26 @@
+import { useCartQuery } from "@/contexts/slices/apiSlice";
+import { faCircleExclamation } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 export function CheckoutTotal() {
+  const cartQuery = useCartQuery({});
+
+  if (!cartQuery.isSuccess) {
+    return (
+      <div className="mt-2 rounded-md border border-gray-300 px-2 py-3">
+        <FontAwesomeIcon icon={faCircleExclamation} className="mr-1" />
+        Failed to load cart
+      </div>
+    );
+  }
+
   return (
     <div className="mt-2 rounded-md border border-gray-300 px-2 py-3">
       <div className="flex items-center justify-between pb-2">
         <span>Subtotal:</span>
-        <span className="font-medium">20$</span>
+        <span className="font-medium">
+          {cartQuery.data.reduce((accumulator, item) => accumulator + item.book.currentPrice * item.quantity, 0).toLocaleString()}
+        </span>
       </div>
       <div className="flex items-center justify-between pb-2">
         <span>Shipping:</span>
